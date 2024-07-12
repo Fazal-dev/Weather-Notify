dc89f5df32c5ea766e77398b9b324c21b30b2eb5
Shain922 <fmhd48763@gmail.com>
2024-07-12 15:32:31 +0530

    add : nodemailer send email

--- cronJob.js
+++ cronJob.js
@@ -1,48 +1,29 @@
-import cron from "node-cron";
-import axios from "axios";
 import nodemailer from "nodemailer";
 import "dotenv/config";
-import UserModel from "./models/userModel.js";
-import WeatherDataModel from "./models/weatherData.js";
 
-const fetchWeatherData = async (location) => {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHER_API_KEY}`;
-  const response = await axios.get(url);
-  return response.data;
+// Create a transporter object
+const transporter = nodemailer.createTransport({
+  host: process.env.MAILTRAP_HOST,
+  port: process.env.MAILTRAP_PORT,
+  auth: {
+    user: process.env.MAILTRAP_USER,
+    pass: process.env.MAILTRAP_PASS,
+  },
+});
+
+// Configure the mailoptions object
+const mailOptions = {
+  from: "hi@demomailtrap.com",
+  to: "mhdm8675@gmail.com",
+  subject: "Sending Email using Node.js",
+  text: "That was easy!",
 };
 
-const sendEmail = async (email, weatherData) => {
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Weather Report",
-    text: `Here is your weather report: ${JSON.stringify(weatherData)}`,
-  };
-
-  await transporter.sendMail(mailOptions);
-};
-
-cron.schedule("*/30 * * * * *", async () => {
-  console.log("Running a task every seconds");
-
-  const users = await UserModel.find();
-  for (const user of users) {
-    const weatherData = await fetchWeatherData(user.location);
-
-    const newWeatherData = new WeatherDataModel({
-      userId: user._id,
-      data: weatherData,
-    });
-    await newWeatherData.save();
-
-    await sendEmail(user.email, weatherData);
+// Send the email
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
   }
 });
